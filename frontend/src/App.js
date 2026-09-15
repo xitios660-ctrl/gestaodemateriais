740525d36fd02ed2399249d00833abd6ac6a0c27
xitios660-ctrl <xitios660@gmail.com>
2026-09-14 21:51:25 -0300

    fix: add safe fallback route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -45,6 +45,7 @@ function App() {
                 </ProtectedRoute>
               }
             />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
